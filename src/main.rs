d4ea291ec3e310f2b66b35b72e3cbe51907c1962
marmalade <summit@tutanota.de>
2018-01-11 13:15:08 -0800

    Added MongoDB. Did some code cleanup.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-#[macro_use] extern crate serenity;
-//#[macro_use(bson, doc)]
+extern crate serenity;
 extern crate yaml_rust;
+#[macro_use(bson, doc)]
 extern crate bson;
 extern crate mongodb;
 
@@ -9,7 +9,10 @@ use serenity::model::*;
 
 use yaml_rust::{YamlLoader,YamlEmitter};
 
-//use std::env;
+use bson::Bson;
+use mongodb::ThreadedClient;
+use mongodb::db::ThreadedDatabase;
+
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -21,14 +24,14 @@ impl EventHandler for Handler {
     // Event handlers are dispatched through multi-threading, and so multiple of a single event can be dispatched simultaneously.
     fn on_message(&self, _: Context, msg: Message) {
         if msg.content == ".ping" {
-            // Sending a message can fail, due to a network error, an authentication error, or lack of permissions to post in the channel, so log to stdout when some error happens, with a description of it.
             if let Err(why) = msg.channel_id.say("pong") {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
-    // Set a handler to be called on the 'on_ready' event. This is called when a shard is booted, and a READY payload is sent by Discord. This payload contains data like the current user's guild Ids, current user data, private channels, and more.
+    // Set a handler to be called on the 'on_ready' event. This is called when a shard is booted, and a READY payload is sent by Discord.
+    // This payload contains data like the current user's guild Ids, current user data, private channels, and more.
     //
     // In this case, just print what the current user's username is.
     fn on_ready (&self, _: Context, ready: Ready) {
@@ -37,7 +40,7 @@ impl EventHandler for Handler {
 }
 
 fn main() {
-    // Read config file (YAML)
+    //  -- Read YAML Config file. --
     let mut config = File::open("config.yaml").expect("config.yaml not found.");
     println!("Reading config from file: {:?}", config);
 
@@ -48,26 +51,45 @@ fn main() {
     let docs = YamlLoader::load_from_str(&configcontents).unwrap();
     let doc = &docs[0];
 
-    // Debug support
-    println!("{:?}", docs);
-
-    //assert_eq!(doc["Token"].as_str().unwrap*(););
+    println!("{:?}", docs); // Debug support
 
     let mut out_str = String::new(); {
         let mut emitter = YamlEmitter::new(&mut out_str);
         emitter.dump(doc).unwrap();
-   }
-   println!("{}", out_str);
-   println!("{:?}", doc["Token"].as_str());
+    }
+    println!("{}", out_str); // Debug support
+    println!("{:?}", doc["Token"].as_str()); // Debug support
 
-    // Configure the client with your Discord bot token in the environment.
-    // let token = env::var("DISCORD_TOKEN")
-    //     .expect("Expected a token in the environment");
     let token = doc["Token"].as_str()
         .expect("Expected a token in config file.");
 
+    let mdb_client = mongodb::Client::connect("localhost", 27017)
+        .expect("Failed to initialize MongoDB standalone client.");
+    let coll = mdb_client.db("test").collection("movies");
+    let mdbdoc = doc! {
+        "title": "Jaws",
+        "array": [1, 2, 3],
+    };
+
+    coll.insert_one(mdbdoc.clone(), None)
+        .ok().expect("Failed to insert document.");
+
+    let mut cursor = coll.find(Some(mdbdoc.clone()), None)
+        .ok().expect("Failed to execute find.");
+
+    let item = cursor.next();
+
+    match item {
+        Some(Ok(mdbdoc)) => match mdbdoc.get("title") {
+            Some(&Bson::String(ref title)) => println!("{}", title),
+            _ => panic!("Expected title to be a string!"),
+        },
+        Some(Err(_)) => panic!("Failed to get next from server!"),
+        None => panic!("Server returned to results!"),
+    }
+
     // Create a new instance of the Client, logging in as a bot. This will automatically prepend your bot token with "Bot ", which is a requirement by discord for bot users.
-    let mut client = Client::new(&token, Handler);
+    let mut client = serenity::Client::new(&token, Handler);
 
     // Finally, start a single shard, and start listening to events.
     //
